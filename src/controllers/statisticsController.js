eb69583ec74939a91694eb0b0d42c11818e621e8
jammal.souheir@gmail.com <jammal.souheir@gmail.com>
2024-01-26 21:20:35 +0200

    getting image with top seller product

--- src/controllers/statisticsController.js
+++ src/controllers/statisticsController.js
@@ -187,7 +187,7 @@ const getTotalOrders=async (req, res) => {
       const topSellerProducts = await Product.find({})
         .sort({ soldQuantityCounter: -1 })
         .limit(5)
-        .select('name soldQuantityCounter');
+        .select('name image soldQuantityCounter');
   
       if (topSellerProducts.length > 0) {
         res.status(200).json({ topSellerProducts });
