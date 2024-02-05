71177fb315bdb30b0c1798bc5af1eda3dc75793d
Hasan Deeb <95194832+HasanDeeb7@users.noreply.github.com>
2024-02-05 20:59:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: process.env.FRONTEND_ORIGIN,
+    origin: "*",
     credentials: true,
     optionsSuccessStatus: 200,
   })
